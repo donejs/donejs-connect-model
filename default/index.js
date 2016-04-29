76f0a43e44b9b7b15793abb78f7631564f108a51
Marshall Thompson <marshall@creativeideal.net>
2016-04-28 18:41:02 -0600

    Add connect-model
    This is just like the supermodel generator with a different main model.js template.

--- default/index.js
+++ default/index.js
@@ -1,33 +1,97 @@
-var generator = require('yeoman-generator');
+var generators = require('yeoman-generator');
+var path = require('path');
+var _ = require('lodash');
+var utils = require('../lib/utils');
+var upperFirst = require("lodash.upperfirst");
+var utils = require('../lib/utils');
 
-module.exports = generator.Base.extend({
-  initializing: function () {
-    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
-    
-    this.files = [
-      'file.js'
+module.exports = generators.Base.extend({
+  templatePath: utils.templatePath(path.join('.donejs', 'templates', 'supermodel')),
+
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+
+    this.props = {};
+
+    this.argument('name', {
+      type: String,
+      required: false,
+      desc: 'The name for the model (e.g. order)'
+    });
+
+    this.modelFiles = [
+      'fixtures/model.js',
+      'model.js',
+      'model_test.js'
     ];
   },
 
   prompting: function () {
     var done = this.async();
 
-    this.prompt([{
-      type    : 'input',
-      name    : 'name',
-      message : 'What is the name of the file?'
-    }], function (answers) {
-      this.props = answers;
-      done();
-    }.bind(this));
+		this.prompt({
+			name: 'name',
+			type: String,
+			required: true,
+			message: 'The name for your model (e.g. order)',
+			when: !this.name
+		}, function (first) {
+			var name = this.name = this.name || first.name;
+			var prompts = [{
+				name: 'url',
+				message: 'What is the URL endpoint?',
+				default: '/' + name
+			}, {
+				name: 'idProp',
+				message: 'What is the property name of the id?',
+				default: 'id'
+			}];
+
+			this.prompt(prompts, function (props) {
+				this.props = _.extend(this.props, props);
+
+				done();
+			}.bind(this));
+		}.bind(this));
   },
+
   writing: function () {
-    this.log('Copying file to ' + this.destinationPath(this.props.name + '.js'));
-    
-    this.fs.copyTpl(
-			this.templatePath('file.js'),
-			this.destinationPath(this.props.name + '.js'),
-			this.props
-		);
+    var self = this;
+    var done = this.async();
+
+    var pkg = utils.getPkgOrBail(this, done);
+    if(!pkg) {
+      return;
+    }
+
+    var folder = _.get(pkg, 'system.directories.lib') || './';
+    var appName = _.get(pkg, 'name');
+
+    var options = {
+      className: upperFirst(_.camelCase(this.name)),
+      name: this.name,
+      url: this.props.url.trim(),
+      idProp: this.props.idProp
+    };
+
+    if(this.name === 'test') {
+      throw new Error('A model cannot be named "test"');
+    }
+
+    this.modelFiles.forEach(function(name) {
+      var target = name.replace('model', options.name);
+      self.fs.copyTpl(
+        self.templatePath(name),
+        self.destinationPath(path.join(folder, 'models', target)),
+        options
+      );
+    });
+
+    var modelTest = this.destinationPath(path.join(folder, 'models', 'test.js'));
+    utils.addImport(modelTest, appName + '/models/' + options.name + '_test');
+
+    var fixturesFile = this.destinationPath(path.join(folder, 'models', 'fixtures', 'fixtures.js'));
+    utils.addImport(fixturesFile, appName + '/models/fixtures/' + options.name);
+    done();
   }
 });
---
+++ default/templates/model.js
@@ -0,0 +1,63 @@
+import can from 'can';
+import $ from 'jquery';
+import connect from 'can-connect';
+import tag from 'can-connect/can/tag/';
+import 'can-connect/constructor/';
+import 'can-connect/can/map/';
+import 'can-connect/can/';
+import 'can-connect/constructor/store/';
+import 'can-connect/constructor/callbacks-once/';
+import 'can-connect/data/callbacks/';
+import 'can-connect/data/callbacks-cache/';
+import 'can-connect/data/combine-requests/';
+import 'can-connect/data/inline-cache/';
+import "can-connect/data/localstorage-cache/";
+import 'can-connect/data/parse/';
+import 'can-connect/data/url/';
+import 'can-connect/fall-through-cache/';
+import 'can-connect/real-time/';
+import 'can/map/define/define';
+
+var behaviors = [
+  'constructor',
+  'can-map',
+  'constructor-store',
+  'data-callbacks',
+  'data-combine-requests',
+  'data-inline-cache',
+  'data-parse',
+  'data-url',
+  'constructor-callbacks-once'
+];
+
+if(typeof localStorage !== "undefined") {
+	if(!options.cacheConnection) {
+		options.cacheConnection = connect(["data-localstorage-cache"],{
+			name: options.name+"Cache",
+			idProp: options.idProp,
+			algebra: options.algebra
+		});
+	}
+	behaviors.push("fall-through-cache");
+}
+
+export const <%= className %> = can.Map.extend(<%= className %>, {
+  define: {}
+});
+
+<%= className %>.List = can.List.extend({
+  Map: <%= className %>
+}, {});
+
+export const <%= name %>Connection = connect(behaviors, {
+  ajax: $.ajax,
+  url: '<%= url %>',
+  idProp: '<%= idProp %>',
+  Map: <%= className %>,
+  List: <%= className %>.List,
+  name: '<%= name %>'
+});
+
+tag('<%= name %>-model', <%= name %>Connection);
+
+export default <%= className %>;
